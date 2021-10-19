d5f49535a862ba2a777f4965efe293c55faae03a
ak1215 <60421957+ak1215@users.noreply.github.com>
2021-10-19 09:43:59 +0200

    Swagger added

--- src/Components/ProductService.tsx
+++ src/Components/ProductService.tsx
@@ -1,23 +1,16 @@
 import axios from "axios";
-<<<<<<< HEAD
-import { url } from "inspector";
+
 import { Values } from "./Contact";
 
 class ProductService {
   private data = axios.create({ baseURL: "http://localhost:8080/" });
-  private productDate = {};
-=======
-
-class ProductService {
-  private data = axios.create({ baseURL: "http://localhost:3000/" });
->>>>>>> 4e77cd3d6f476fd9da641515a3db418d922d7983
 
   public getAllProducts() {
     return this.data.get("products/");
   }
 
   public getProductById(id: number) {
-<<<<<<< HEAD
+
     return this.data.get("products/" + id);
   }
 
@@ -28,9 +21,6 @@ class ProductService {
   public addProduct(values: Values){
     return this.data.post("/person", values)
 
-=======
-    return this.data.get("/products/" + id);
->>>>>>> 4e77cd3d6f476fd9da641515a3db418d922d7983
   }
 }
 export default ProductService;
--- webShop/src/main/java/com/example/webShop/WebShopApplication.java
+++ webShop/src/main/java/com/example/webShop/WebShopApplication.java
@@ -3,15 +3,39 @@ package com.example.webShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class WebShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebShopApplication.class, args);
 	}
+
 	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.webShop")).build().apiInfo(apiDetails());
+	}
+	private ApiInfo apiDetails(){
+		return new ApiInfo(
+				"Webshop",
+				"",
+				"",
+				"",
+				new springfox.documentation.service.Contact("Anushya", "http://localhost:3000/", "anushya.nagulanandan@noseryoung.ch"),
+				"API",
+				"",
+				Collections.emptyList());
+	}
+/*	@Bean
 	public CommonsRequestLoggingFilter logFilter() {
 		CommonsRequestLoggingFilter filter
 				= new CommonsRequestLoggingFilter();
@@ -21,5 +45,5 @@ public class WebShopApplication {
 		filter.setIncludeHeaders(false);
 		filter.setAfterMessagePrefix("REQUEST DATA : ");
 		return filter;
-	}
+	}*/
 }
